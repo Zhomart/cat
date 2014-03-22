c2c857f560a2b3314767bbcd8d511735d1658b44
Zhomart Mukhamejanov <mzhomart@gmail.com>
2014-03-21 19:22:28 -0700

    added staging env

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Cat::Application.routes.draw do
   resources :girls
 
+  root to: 'welcome#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
