796cb80d511be2826a11ec3f749a96366c0e7d33
Zhomart Mukhamejanov <mzhomart@gmail.com>
2014-03-23 21:11:23 -0700

    show only allowed env keys

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,7 @@
 class WelcomeController < ApplicationController
+  KEYS = %w(RAILS_ENV RACK_ENV PATH DYNO PORT HOSTNAME WAT)
+
   def index
+    @env = ENV.select { |k, v| KEYS.include?(k.to_s) }
   end
 end
